4a38ab7203ea884f6b23799f835c3447f1a9f188
91950 <91950@LAPTOP-BUTDSDIS>
2022-03-23 16:04:43 +0530

    First commit.....

---
+++ src/GitDemoProject/Test.java
@@ -0,0 +1,20 @@
+package GitDemoProject;
+
+public class Test 
+{
+public static void main(String[] args) 
+{
+String hungry = "yes";
+
+if(hungry == "yes")
+{
+System.out.println("eat something....");
+}
+else
+{
+System.out.println("Do work....");	
+}
+}
+	
+}
+
---
+++ src/GitDemoProject/Test2.java
@@ -0,0 +1,20 @@
+package GitDemoProject;
+
+public class Test2 
+{
+public static void main(String[] args) 
+{
+String hungry = "yes";
+
+if(hungry == "yes")
+{
+System.out.println("eat something....");
+}
+else
+{
+System.out.println("Do work....");	
+}
+}
+	
+}
+
